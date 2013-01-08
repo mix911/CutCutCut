861a5ac5b0dcced85e7b78ba9b0aab404a2cec4e
mix911 <wmihanale@gmail.com>
2013-01-08 07:02:43 +0400

    Initial Commit

---
+++ CutCutCut/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  CutCutCut
+//
+//  Created by demo on 08.01.13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
